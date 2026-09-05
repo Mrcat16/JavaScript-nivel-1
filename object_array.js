5e1d4d9f0f3dd42cfa6857a268f52ae1f885169b
Mrcat16 <ID+Mrcat16@users.noreply.github.com>
2026-09-04 19:58:04 -0500

    xd

--- object_array.js
+++ object_array.js
@@ -1,6 +1,12 @@
 const object = {
-    nombre = "Jorge",
-    apellido = "Lambis"
+    nombre: "Jorge",
+    apellido: "Lambis"
 }
-object.edad(`18`)
+object.edad = 18
 console.log(object)
+
+const array = [32, 12, 75]
+
+array.push(16)
+array.sort((a,b) => a - b)
+console.log(array)
