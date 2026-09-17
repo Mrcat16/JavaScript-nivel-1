f7aa49c2707b930c5d51fb73e7ea7c4cd867d7ec
Mrcat16 <309990886+Mrcat16@users.noreply.github.com>
2026-09-17 16:06:03 -0500

    xd

--- problema1.js
+++ problema1.js
@@ -1,14 +1,59 @@
 const prompt = require("prompt-sync") ();
-const hogar = () => {
+const luzz = (arr) => {
+    for (let i = 1; i < 2; i++) {
     try {
-        const gastos = Number(prompt(`Cuantos gastas en luz al mes?`))
-    if (isNaN)(gastos) => {
-        throw new Error(`Error dato no valida ingrese solo numeros porfa`)
-        
+        const luz = Number(prompt(`Cuanto le viene el recibo del luz al mes? `))
+    if (isNaN(luz)) {
+        throw new Error(`Error dato no valida solo ingrese numeros`)
     }
-    hogar.push(gastos)
+    arr.push(luz)
     }
     catch(error) {
         console.log(error.message)
+        i--
     }
 }
+}
+const aguaa = (arr1) => {
+    for (let i = 1; i < 2; i++) {
+    try {
+        const agua = Number(prompt(`Cuanto le viene el recibo del agua al mes? `))
+    if (isNaN(agua)) {
+        throw new Error(`Eroor dato no valido solo ingrese numeros`)
+    }
+    arr1.push(agua)
+    }
+    catch(error) {
+        console.log(error.message)
+        i--
+    }
+    }
+}
+const gass = (arr2) => {
+    for (let i = 1; i < 2; i++) {
+    try {
+        const gas = Number(prompt(`Cuanto le viene el recibo del gas al mes? `))
+    if (isNaN(gas)) {
+        throw new Error(`Error dato no valido solo ingrese numeros`)   
+    }
+    arr2.push(gas)
+    }
+    catch (error) {
+        console.log(error.message)
+        i--
+    }
+    }
+}
+let lista_luz = []
+luzz(lista_luz)
+let lista_agua = []
+aguaa(lista_agua)
+let lista_gas = []
+gass(lista_gas)
+let total = [...lista_luz,...lista_agua,...lista_gas]
+.reduce((suma,valor) => suma + valor, 0);
+console.log(`\nEsto es lo que gasta al mes en luz $${lista_luz} dolares\n`)
+console.log(`Esto es lo que gasta al mes en agua $${lista_agua} dolares\n`)
+console.log(`Esto es lo que gaste de gas al mes $${lista_gas} dolares\n`)
+console.log(`Total de lo que gasta al mes $${total} dolares\n`)
+
